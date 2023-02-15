df9cd5db97970280df6d7f4afcf6c5b786e699ba
bordeidenis <97630007+bordeidenis@users.noreply.github.com>
2023-02-15 16:24:26 +0200

    Update Задача1.6.2.sh

--- Задача1.6.2.sh
+++ Задача1.6.2.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-#echo "Enter note:" 
-#read string > file228.txt
-#echo "Enter any number:"
-#read int > file228.txt
-#time=$(date "+%Y-%m-%d-%H:%M:%S")
-#echo "$time"
 echo "Enter note:"
 read hello
 echo "Enter any number:"
